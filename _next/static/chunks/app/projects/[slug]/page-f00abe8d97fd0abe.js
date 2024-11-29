63ba4ddbb2f2243600543a39d190754d763e8d8d
yosefa <yosefa71@gmail.com>
2024-11-29 21:08:30 +0700

    updates

---
+++ _next/static/chunks/app/projects/[slug]/page-f00abe8d97fd0abe.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[419],{6320:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7208,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[208,465,743,358],()=>s(6320)),_N_E=e.O()}]);
